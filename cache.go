c51f1e560ecc2e46703afa8aca34806e446a4376
urcop <kirillzama@list.ru>
2023-04-28 20:54:35 +0300

    Added ttl to cache and rewrite some features

--- cache.go
+++ cache.go
@@ -1,35 +1,66 @@
 package cache
 
+import (
+	"sync"
+	"time"
+)
+
+type value struct {
+	value interface{}
+	ttl   *time.Time
+}
+
 type Cache struct {
-	data map[string]interface{}
+	ticker *time.Ticker
+	data   sync.Map
+	ttl    time.Duration
 }
 
-func New() *Cache {
+func New(ttl time.Duration) *Cache {
 	return &Cache{
-		data: make(map[string]interface{}),
+		ticker: time.NewTicker(ttl),
+		data:   sync.Map{},
+		ttl:    ttl,
 	}
 }
 
-func (c *Cache) Get(key string) interface{} {
-	exist := c.isExists(key)
-	if exist {
-		return c.data[key]
+func (c *Cache) backgroundCache() {
+	for {
+		<-c.ticker.C
+		c.data.Range(func(key, v interface{}) bool {
+			value, ok := v.(*value)
+			if !ok {
+				return true
+			}
+			if value.ttl == nil {
+				return true
+			}
+			if time.Now().After(*value.ttl) {
+				c.data.Delete(key)
+			}
+			return true
+		})
 	}
-	return nil
+}
+func (db *Cache) Get(key interface{}) (result interface{}, ok bool) {
+	load, ok := db.data.Load(key)
+	if !ok {
+		return nil, false
+	}
+
+	val, ok := load.(*value)
+	if !ok {
+		return nil, false
+	}
+
+	return val.value, true
 }
 
-func (c *Cache) Set(key string, value interface{}) {
-	c.data[key] = value
+func (c *Cache) Set(key string, v interface{}, ttl time.Duration) {
+	t := time.Now().Add(c.ttl)
+	c.data.Store(key, &value{v, &t})
 }
 
 func (c *Cache) Delete(key string) {
-	delete(c.data, key)
-}
-
-func (c *Cache) isExists(key string) bool {
-	_, ok := c.data[key]
-	if !ok {
-		return false
-	}
-	return true
+	c.data.Delete(key)
 }
